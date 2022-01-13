8195d0c2a06c5d22618dd0c29befb71e0d2fa4b7
Thiago Gois <thiagogoisjunior@gmail.com>
2022-01-13 17:51:23 -0300

    snake game

--- DbCreate.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='root', password='********', host='127.0.0.1')
-
-mycursor = cnx.cursor()
-mycursor.execute("CREATE DATABASE mydatabase")
